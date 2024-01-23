6a719ebba5c950b9a7b1c653ed4de8231fae2ca9
kharethkaaaa <156535984+kharethkaaaa@users.noreply.github.com>
2024-01-23 11:44:12 +0100

    idk zkouska cartu

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+import newShoeItem from "./item.js";
+
+const hoodies = [
+    {
+      name:"Hoodie V1",
+      price:40,
+      Image: "assets/img/black_hoodie_front_view.png"
+    }];
+
+const tshirts = [
+    {
+        name: "tshirt V1", 
+        price: 35,
+        Image: "assets/img/black_hoodie_front_view.png"
+    }];
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,44 @@
-function zoom(e){
+/*function zoom(e){
     var zoomer = e.currentTarget;
     e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
     e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
     x = offsetX/zoomer.offsetWidth*100
     y = offsetY/zoomer.offsetHeight*100
     zoomer.style.backgroundPosition = x + '% ' + y + '%';
-  }
+  }*/
+
+document.addEventListener("DOMContentLoaded", addItemToCart);
+
+function addItemToCart() {
+    const addSelectedShoes = document.getElementById("add-items");
+    shoes.forEach(shoe => {
+        addSelectedShoes.appendChild(shoeItem(shoe));
+    })
+}
 
 (function ($) {
     "use strict";
 
-    $(document).ready(function($){
-        
+    $(document).ready(function ($) {
+
         // testimonial sliders
         $(".testimonial-sliders").owlCarousel({
             items: 1,
             loop: true,
             autoplay: true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
                 },
-                600:{
-                    items:1,
-                    nav:false
+                600: {
+                    items: 1,
+                    nav: false
                 },
-                1000:{
-                    items:1,
-                    nav:false,
-                    loop:true
+                1000: {
+                    items: 1,
+                    nav: false,
+                    loop: true
                 }
             }
         });
@@ -42,21 +51,21 @@ function zoom(e){
             nav: true,
             dots: false,
             navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false,
-                    loop:true
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: true
                 },
-                600:{
-                    items:1,
-                    nav:true,
-                    loop:true
+                600: {
+                    items: 1,
+                    nav: true,
+                    loop: true
                 },
-                1000:{
-                    items:1,
-                    nav:true,
-                    loop:true
+                1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true
                 }
             }
         });
@@ -67,36 +76,36 @@ function zoom(e){
             loop: true,
             autoplay: true,
             margin: 30,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
                 },
-                600:{
-                    items:3,
-                    nav:false
+                600: {
+                    items: 3,
+                    nav: false
                 },
-                1000:{
-                    items:4,
-                    nav:false,
-                    loop:true
+                1000: {
+                    items: 4,
+                    nav: false,
+                    loop: true
                 }
             }
         });
 
         // count down
-        if($('.time-countdown').length){  
-            $('.time-countdown').each(function() {
-            var $this = $(this), finalDate = $(this).data('countdown');
-            $this.countdown(finalDate, function(event) {
-                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+        if ($('.time-countdown').length) {
+            $('.time-countdown').each(function () {
+                var $this = $(this), finalDate = $(this).data('countdown');
+                $this.countdown(finalDate, function (event) {
+                    var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+                });
             });
-         });
         }
 
         // projects filters isotop
         $(".product-filters li").on('click', function () {
-            
+
             $(".product-filters li").removeClass("active");
             $(this).addClass("active");
 
@@ -105,9 +114,9 @@ function zoom(e){
             $(".product-lists").isotope({
                 filter: selector,
             });
-            
+
         });
-        
+
         // isotop inner
         $(".product-lists").isotope();
 
@@ -132,19 +141,19 @@ function zoom(e){
         });
 
         // homepage slides animations
-        $(".homepage-slider").on("translate.owl.carousel", function(){
-            $(".hero-text-tablecell .subtitle").removeClass("animated fadeInUp").css({'opacity': '0'});
-            $(".hero-text-tablecell h1").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
-            $(".hero-btns").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        $(".homepage-slider").on("translate.owl.carousel", function () {
+            $(".hero-text-tablecell .subtitle").removeClass("animated fadeInUp").css({ 'opacity': '0' });
+            $(".hero-text-tablecell h1").removeClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.3s' });
+            $(".hero-btns").removeClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.5s' });
         });
 
-        $(".homepage-slider").on("translated.owl.carousel", function(){
-            $(".hero-text-tablecell .subtitle").addClass("animated fadeInUp").css({'opacity': '0'});
-            $(".hero-text-tablecell h1").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
-            $(".hero-btns").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        $(".homepage-slider").on("translated.owl.carousel", function () {
+            $(".hero-text-tablecell .subtitle").addClass("animated fadeInUp").css({ 'opacity': '0' });
+            $(".hero-text-tablecell h1").addClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.3s' });
+            $(".hero-btns").addClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.5s' });
         });
 
-       
+
 
         // stikcy js
         $("#sticker").sticky({
@@ -156,20 +165,20 @@ function zoom(e){
             meanMenuContainer: '.mobile-menu',
             meanScreenWidth: "992"
         });
-        
-         // search form
-        $(".search-bar-icon").on("click", function(){
+
+        // search form
+        $(".search-bar-icon").on("click", function () {
             $(".search-area").addClass("search-active");
         });
 
-        $(".close-btn").on("click", function() {
+        $(".close-btn").on("click", function () {
             $(".search-area").removeClass("search-active");
         });
-    
+
     });
 
 
-    jQuery(window).on("load",function(){
+    jQuery(window).on("load", function () {
         jQuery(".loader").fadeOut(1000);
     });
 
